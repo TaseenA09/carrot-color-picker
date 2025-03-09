fea203a428b4e269ff5247b3031ece541590d9ba
TaseenA09 <taseenahnaf140@gmail.com>
2025-03-09 16:18:12 +0600

    Build Update

--- build/build.mjs
+++ build/build.mjs
@@ -5,6 +5,7 @@ import fs from 'fs';
 let result = await esbuild.build({
     entryPoints: ['src/*.js', 'src/*.css'],
     outdir: './',
+    bundle: true,
     treeShaking: true,
     minify: true,
 })
